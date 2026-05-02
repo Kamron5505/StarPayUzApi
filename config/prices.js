bc71164c024f3a57a3a61bd2b3aff46d0cd1bde9
Kamron5505 <kama58077@gmail.com>
2026-05-02 21:28:42 +0500

    Fix: Star prices to 195 UZS per star (Node.js API)

--- config/prices.js
+++ config/prices.js
@@ -1,20 +1,20 @@
 /**
  * Star packages price list (in UZS)
- * Rate: 1 star = 225 UZS
+ * Rate: 1 star = 195 UZS
  * Min: 50 stars | Max: 5000 stars
  */
-const PRICE_PER_STAR_UZS = 200;
+const PRICE_PER_STAR_UZS = 195;
 const MIN_STARS = 50;
 const MAX_STARS = 5000;
 
 const STAR_PACKAGES = [
-  { stars: 50,   price_uzs: 50   * PRICE_PER_STAR_UZS }, // 11 250
-  { stars: 100,  price_uzs: 100  * PRICE_PER_STAR_UZS }, // 22 500
-  { stars: 250,  price_uzs: 250  * PRICE_PER_STAR_UZS }, // 56 250
-  { stars: 500,  price_uzs: 500  * PRICE_PER_STAR_UZS }, // 112 500
-  { stars: 1000, price_uzs: 1000 * PRICE_PER_STAR_UZS }, // 225 000
-  { stars: 2500, price_uzs: 2500 * PRICE_PER_STAR_UZS }, // 562 500
-  { stars: 5000, price_uzs: 5000 * PRICE_PER_STAR_UZS }, // 1 125 000
+  { stars: 50,   price_uzs: 50   * PRICE_PER_STAR_UZS }, // 9 750
+  { stars: 100,  price_uzs: 100  * PRICE_PER_STAR_UZS }, // 19 500
+  { stars: 250,  price_uzs: 250  * PRICE_PER_STAR_UZS }, // 48 750
+  { stars: 500,  price_uzs: 500  * PRICE_PER_STAR_UZS }, // 97 500
+  { stars: 1000, price_uzs: 1000 * PRICE_PER_STAR_UZS }, // 195 000
+  { stars: 2500, price_uzs: 2500 * PRICE_PER_STAR_UZS }, // 487 500
+  { stars: 5000, price_uzs: 5000 * PRICE_PER_STAR_UZS }, // 975 000
 ];
 
 const getPriceForStars = (stars) => {
